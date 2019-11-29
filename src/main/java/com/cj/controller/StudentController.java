fa45a46b0ddb17a609adb9a44063c2066c383b14
PatyQ <qxy136181378@gmail.com>
2019-11-29 16:21:55 +0800

    springBoot测试

--- src/main/java/com/cj/controller/StudentController.java
+++ src/main/java/com/cj/controller/StudentController.java
@@ -4,7 +4,9 @@ import com.cj.entity.StudentEntity;
 import com.cj.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,8 +24,23 @@ public class StudentController {
         List<StudentEntity> list = studentService.list();
         System.out.println(list);
         map.put("userlist",list);
-
         return "studentlist";
+    }
+
+    @RequestMapping("selUserById/{id}")
+    private String selUserById (@PathVariable Integer id, ModelMap map){
+        StudentEntity studentEntity = studentService.getById(id);
+        System.out.println(studentEntity);
+        map.put("stu",studentEntity);
+        return "updateStu";
+    }
+
+    @RequestMapping("updUser")
+    private String updUserById (StudentEntity studentEntity, ModelMap map){
+        System.out.println(studentEntity);
+
+        map.put("stu",studentEntity);
+        return "updateStu";
     }
 
 }
--- src/main/java/com/cj/entity/StudentEntity.java
+++ src/main/java/com/cj/entity/StudentEntity.java
@@ -1,6 +1,7 @@
 package com.cj.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
@@ -11,6 +12,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -26,6 +28,10 @@ public class StudentEntity implements Serializable {
     private String name;
 
     private Integer sex;
+
+    //若没有开启驼峰命名，或者表中列名不符合驼峰规则，可通过该注解指定数据库表中的列名，exist标明数据表中有没有对应列
+    @TableField(value = "birthday",exist = true)
+    private Date time;
 
 //    private List<String> stus;
 //
